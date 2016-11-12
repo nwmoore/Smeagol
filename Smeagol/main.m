665ff6eadae96f15539d1ee38a61fe63ec4537aa
Lab User <physicsadmin@landau-5138.local>
2016-11-11 20:46:02 -0500

    Initial Commit

---
+++ Smeagol/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Smeagol
+//
+//  Created by Physics Admin on 11/11/16.
+//  Copyright © 2016 PhysicsTeam398. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
